640a96cd2af450eca8772d44b9f53a6cd67133dd
beth <zhangbaige@innobuddy.com>
2019-01-18 16:20:47 +0800

    添加基础测试代码

---
+++ components/Demo/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GroundImage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>demo</div>;
+  }
+}
+
+export default Demo;
